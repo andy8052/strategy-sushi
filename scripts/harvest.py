afaf9a0e670192d660bedade08f45129d704fe41
andy8052 <andychorlian@gmail.com>
2020-11-12 16:38:48 -0500

    feat: add harvest
    feat: add harvest

---
+++ scripts/harvest.py
@@ -0,0 +1,31 @@
+import json
+
+from brownie import StrategySushiswapPair, Vault, accounts, rpc, web3, Wei
+from click import secho
+from eth_utils import is_checksum_address
+
+
+def get_address(label):
+    addr = input(f"{label}: ")
+    if is_checksum_address(addr):
+        return addr
+    resolved = web3.ens.address(addr)
+    if resolved:
+        print(f"{addr} -> {resolved}")
+        return resolved
+    raise ValueError("invalid address or ens")
+
+
+def main():
+    deployer = accounts.load(input("deployer account: "))
+
+    strategy = StrategySushiswapPair.at(get_address("strat"))
+
+    balance = strategy.estimatedTotalAssets().to("ether")
+
+    strategy.harvest({"from": deployer})
+
+    secho(
+        f"Total Assets: {balance} SLP\nstrategy: {strategy}\n",
+        fg="green",
+    )
